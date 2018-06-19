1200e507744d3e80db52fbdc1a9ca4a59fba895b
Gregory Way <gregory.way@gmail.com>
2018-06-19 16:17:51 -0700

    add updated analysis
    add shuffle command
    
    Update Ensemble Z Matrix Search (#22)
    
    * add in dataset option
    
    * add updated analysis
    
    * add dataset arg
    
    * update parameter sweep files
    
    * remove legacy code
    
    * remove arg flag colision
    
    * fix unnamed columns
    
    * update tybalt hash in env
    
    * update saving plots

--- 2.ensemble-z-analysis/analysis.sh
+++ 2.ensemble-z-analysis/analysis.sh
@@ -1,12 +1,55 @@
 #!/bin/bash
 
-param_file='config/full_z_sweep_params.tsv'
-pmacs_config='../config/pmacs_config.tsv'
-out_dir='results'
-python_path='python'
-num_seeds=10
+# Fit several compression algorithms on various sets of input data with
+# increasing dimensionality.
 
-# Perform ensemble z sweep over several latent space dimensionality estimates
-python scripts/z_sweep_jobs_submit.py --local --param_config $param_file --pmacs_config $pmacs_config \
-        --out_dir $out_dir --python_path $python_path --num_seeds $num_seeds --subset_mad_genes 8000 \
-        --components 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50 60 70 80 90 100 125 150 200
+# Note that the optimal hyperparameters for each dimension are stored in the
+# dataset specific configuration files that were previously determined to be
+# optimal in module `1.initial-z-sweep`.
+
+# Note this was run at the PMACS cluster at University of Pennsvylvania on a
+# cluster of 8 NVIDIA GEFORCE GTX Ti GPUs.
+
+# Set Constants
+PARAM_FILE_PREFIX='config/z_parameter_sweep_'
+PMACS_FILE='../config/pmacs_config.tsv'
+PYTHON_PATH='python'
+NUM_SEEDS=10
+NUM_GENES=8000
+DATASETS=( 'TCGA' 'TARGET' 'GTEX')
+
+# Loop through each dataset and perform analysis
+for dat in "${DATASETS[@]}"
+do
+    PARAM_FILE=$PARAM_FILE_PREFIX$dat'.tsv'
+    OUT_DIR='results/'$dat'_results/'
+
+    python scripts/z_sweep_jobs_submit.py \
+          --param_config $PARAM_FILE \
+          --pmacs_config $PMACS_FILE \
+          --out_dir $OUT_DIR \
+          --num_seeds $NUM_SEEDS \
+          --subset_mad_genes $NUM_GENES \
+          --components 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50 60 70 80 90 100 125 150 200 \
+          --dataset $dat \
+          --local
+done
+
+# Loop through all datasets again with shuffled gene expression
+for dat in "${DATASETS[@]}"
+do
+    PARAM_FILE=$PARAM_FILE_PREFIX$dat'.tsv'
+    OUT_DIR='results/'$dat'_shuffled_results/'
+
+    python scripts/z_sweep_jobs_submit.py \
+          --param_config $PARAM_FILE \
+          --pmacs_config $PMACS_FILE \
+          --out_dir $OUT_DIR \
+          --num_seeds $NUM_SEEDS \
+          --subset_mad_genes $NUM_GENES \
+          --components 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50 60 70 80 90 100 125 150 200 \
+          --dataset $dat \
+          --local \
+          --shuffle
+done
+

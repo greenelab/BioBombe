cefdbec21ab7d9ed60b737d0460f73c49329e952
gwaygenomics <gregory.way@gmail.com>
2018-12-13 10:52:26 -0500

    add unconverged DAE to summary figure

--- 1.initial-z-sweep/scripts/nbconverted/visualize-parameter-sweep.r
+++ 1.initial-z-sweep/scripts/nbconverted/visualize-parameter-sweep.r
@@ -256,31 +256,38 @@ plotBestModel(target_adage_good_training_df,
               algorithm = "ADAGE",
               output_fig_dir = target_fig_dir) + theme(text = element_text(size = 15))
 
-adage_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'DAE', hjust = 1, vjust = 0.8)
-tybalt_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'VAE', hjust = 1, vjust = 0.8)
-gtex_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'GTEX', angle = 90, vjust = 1)
-tcga_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'TCGA', angle = 90, vjust = 1)
-target_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'TARGET', angle = 90, vjust = 1)
+adage_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'DAE', hjust = 1, vjust = 0.4)
+adage_converge_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'DAE (converged)',
+                                                                hjust = 0.7, vjust = 0.4)
+tybalt_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'VAE', hjust = 1, vjust = 0.4)
+gtex_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'GTEX', angle = 90, hjust = 0.8, vjust = 0)
+tcga_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'TCGA', angle = 90, hjust = 0.8, vjust = 0)
+target_label <- cowplot::ggdraw() + cowplot::draw_label(label = 'TARGET', angle = 90, hjust = 0.5, vjust = 16)
 
-alg_main <- cowplot::plot_grid(adage_label, tybalt_label, ncol = 2)
-dataset_main <- cowplot::plot_grid(gtex_label, tcga_label, target_label, nrow = 3)
+alg_main <- cowplot::plot_grid(adage_label, adage_converge_label, tybalt_label, ncol = 3)
+dataset_main <- cowplot::plot_grid(gtex_label, tcga_label, cowplot::ggdraw(), nrow = 3)
 
 main_plot <- (
     cowplot::plot_grid(
-        gtex_adage_converge + ggtitle('') + xlab('') +
+        gtex_adage$final_val_plot + ggtitle('') + xlab('') +
+            theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
+        gtex_adage_converge + ggtitle('') + xlab('') + ylab('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
         gtex_tybalt$final_val_plot + ggtitle('') + ylab('') + xlab('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
-        tcga_adage_converge + ggtitle('') + xlab('') +
+        tcga_adage$final_val_plot + ggtitle('') + xlab('') +
+            theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
+        tcga_adage_converge + ggtitle('') + xlab('') + ylab('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
         tcga_tybalt$final_val_plot + ggtitle('') + ylab('') + xlab('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
+        target_label,
         target_adage$final_val_plot + ggtitle('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
         target_tybalt$final_val_plot + ggtitle('') + ylab('') +
             theme(plot.margin = margin(t = 0, r = 0, b = 0, l = 0)),
-        labels = c("A", "B", "C", "D", "E", "F"),
-        ncol = 2,
+        labels = c("A", "B", "C", "D", "E", "F", "", "G", "H"),
+        ncol = 3,
         nrow = 3
     )
 )
@@ -290,17 +297,17 @@ main_plot <- (
 main_with_alg <- cowplot::plot_grid(alg_main, main_plot, nrow = 2,
                                     rel_heights = c(0.04, 1))
 
-full_plot <- cowplot::plot_grid(dataset_main, main_with_alg, rel_widths = c(0.1, 1))
+full_plot <- cowplot::plot_grid(dataset_main, main_with_alg, rel_widths = c(0.04, 1))
 full_plot
 
 sup_file <- file.path("figures", "z_dimension_sweep_summary.png")
 cowplot::save_plot(filename = sup_file,
                    plot = full_plot,
-                   base_height = 9,
-                   base_width = 10)
+                   base_height = 9.5,
+                   base_width = 16.5)
 
 sup_file <- file.path("figures", "z_dimension_sweep_summary.pdf")
 cowplot::save_plot(filename = sup_file,
                    plot = full_plot,
-                   base_height = 9,
-                   base_width = 10)
+                   base_height = 9.5,
+                   base_width = 16.5)

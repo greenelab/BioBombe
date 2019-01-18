91677637fc503d5f5b3fe8b95e1dd2a80c125d95
gwaygenomics <gregory.way@gmail.com>
2019-01-18 15:33:03 -0500

    update plot dimensions and text size

--- 7.analyze-coverage/scripts/nbconverted/visualize_coverage.r
+++ 7.analyze-coverage/scripts/nbconverted/visualize_coverage.r
@@ -5,15 +5,25 @@ suppressPackageStartupMessages(library(cowplot))
 
 # Set ggplot theme
 coverage_theme <- theme_bw() + 
-  theme(axis.text.x = element_text(angle = 90, size = 7),
-        axis.text.y = element_text(size = 9),
-        plot.title = element_text(hjust = 0.5, size = 12),
-        legend.text = element_text(size = 10),
+  theme(axis.text.x = element_text(angle = 90, size = 3.5),
+        axis.text.y = element_text(size = 6),
+        axis.title = element_text(size = 7),
+        plot.title = element_text(hjust = 0.5, size = 8),
+        legend.text = element_text(size = 7),
+        legend.title = element_text(size = 8),
         legend.key.size = unit(1, "lines"))
 
-algorithms <- c("pca", "ica", "nmf", "dae", "vae")
+algorithm_plot_labels <- c("pca" = "PCA",
+                           "ica" = "ICA",
+                           "nmf" = "NMF",
+                           "dae" = "DAE",
+                           "vae" = "VAE")
+algorithm_colors <- c("#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00")
+algorithms <- names(algorithm_plot_labels)
 
-plot_info_list <- list(c("TCGA", "GpC5BP"), c("TCGA", "GpC4CM"), c("GTEX", "GpXCELL"))
+plot_info_list <- list(c("TCGA", "GpC5BP"),
+                       c("TCGA", "GpC4CM"),
+                       c("GTEX", "GpXCELL"))
 plot_list <- list()
 
 for (plot_info in plot_info_list) {
@@ -42,6 +52,7 @@ for (plot_info in plot_info_list) {
                                     dataset = readr::col_character(),
                                     geneset_name = readr::col_character()
                                 ))
+
     model_df$algorithm <- factor(model_df$algorithm, levels = algorithms)
     model_df$z_dim <-
       factor(model_df$z_dim,
@@ -57,6 +68,7 @@ for (plot_info in plot_info_list) {
                                        dataset = readr::col_character(),
                                        geneset_name = readr::col_character()
                                     ))
+
     ensemble_df$algorithm <- factor(ensemble_df$algorithm, levels = algorithms)
     ensemble_df$z_dim <-
       factor(ensemble_df$z_dim,
@@ -88,6 +100,7 @@ for (plot_info in plot_info_list) {
                                           dataset = readr::col_character(),
                                           geneset_name = readr::col_character()
                                       ))
+
     top_results_df$algorithm <- factor(top_results_df$algorithm, levels = algorithms)
     top_results_df$z_dim <-
       factor(top_results_df$z_dim,
@@ -104,40 +117,31 @@ for (plot_info in plot_info_list) {
     
     # Generate Plots
     plot_list[[plot_identifier]][['model_gg']] <-
-        ggplot(model_df, aes(x = z_dim, y = geneset_coverage * 100)) +
-            geom_boxplot(aes(color = algorithm), outlier.size = 0.15, lwd = 0.23) +
+        ggplot(model_df, aes(x = z_dim,
+                             y = geneset_coverage * 100)) +
+            geom_boxplot(aes(color = algorithm),
+                         outlier.size = 0.15,
+                         lwd = 0.23) +
             coverage_theme +
             scale_color_manual(name = "Algorithm",
-                               values = c("#e41a1c",
-                                          "#377eb8",
-                                          "#4daf4a",
-                                          "#984ea3",
-                                          "#ff7f00"),
-                               labels = c("pca" = "PCA",
-                                          "ica" = "ICA",
-                                          "nmf" = "NMF",
-                                          "dae" = "DAE",
-                                          "vae" = "VAE")) +
+                               values = algorithm_colors,
+                               labels = algorithm_plot_labels) +
             xlab("Z Dimension") +
             ylab("Coverage (%)") +
             ggtitle("Individual Models")
 
     plot_list[[plot_identifier]][['ensemble_gg']] <-
-        ggplot(ensemble_df, aes(x = z_dim, y = geneset_coverage * 100)) +
-            geom_point(aes(color = algorithm, size = log10(abs_top_z_score)), alpha = 0.3) +
-            geom_point(aes(color = algorithm), size = 0.15) +
+        ggplot(ensemble_df, aes(x = z_dim,
+                                y = geneset_coverage * 100)) +
+            geom_point(aes(color = algorithm,
+                           size = log10(abs_top_z_score)),
+                       alpha = 0.3) +
+            geom_point(aes(color = algorithm),
+                       size = 0.15) +
             coverage_theme +
             scale_color_manual(name = "Algorithm",
-                               values = c("#e41a1c",
-                                          "#377eb8",
-                                          "#4daf4a",
-                                          "#984ea3",
-                                          "#ff7f00"),
-                               labels = c("pca" = "PCA",
-                                          "ica" = "ICA",
-                                          "nmf" = "NMF",
-                                          "dae" = "DAE",
-                                          "vae" = "VAE")) +
+                               values = algorithm_colors,
+                               labels = algorithm_plot_labels) +
             xlab("Z Dimension") +
             ylab("Coverage (%)") +
             ggtitle("Ensemble Models")
@@ -154,16 +158,8 @@ for (plot_info in plot_info_list) {
             xlab("Z Dimension") +
             ggtitle("All Models")+
             scale_fill_manual(name = "Algorithm",
-                               values = c("#e41a1c",
-                                          "#377eb8",
-                                          "#4daf4a",
-                                          "#984ea3",
-                                          "#ff7f00"),
-                               labels = c("pca" = "PCA",
-                                          "ica" = "ICA",
-                                          "nmf" = "NMF",
-                                          "dae" = "DAE",
-                                          "vae" = "VAE"))
+                               values = algorithm_colors,
+                               labels = algorithm_plot_labels)
     
     # Add specific scale to certain plots
     if (base_file != "tcga_gpc5bp.tsv") {
@@ -181,24 +177,40 @@ for (plot_info in plot_info_list) {
     
 }
 
-legend <- get_legend(plot_list[['TCGAGpC5BP']][['all_gg']])
+legend <- get_legend(plot_list[['TCGAGpC5BP']][['all_gg']] +
+                     theme(legend.position = "bottom"))
 
 tcga_go_label <- cowplot::ggdraw() +
-    cowplot::draw_label(label = 'TCGA\nGO Biological Processes',
+    cowplot::draw_label(label = 'TCGA\n(GO Biological Processes)',
+                        size = 8,
+                        hjust = 0.4,
                         angle = 90)
 tcga_cm_label <- cowplot::ggdraw() +
-    cowplot::draw_label(label = 'TCGA\nCancer Modules',
+    cowplot::draw_label(label = '     TCGA\n(Cancer Modules)',
+                        size = 8,
+                        hjust = 0.2,
                         angle = 90)
 gtex_label <- cowplot::ggdraw() +
-    cowplot::draw_label(label = 'GTEX\nxCell Cell Types',
+    cowplot::draw_label(label = '        GTEX\n(xCell Cell Types)',
+                        size= 8,
+                        hjust = 0.05,
                         angle = 90)
-dataset_main <- cowplot::plot_grid(tcga_go_label, tcga_cm_label, gtex_label, nrow = 3)
+
+dataset_main <- cowplot::plot_grid(tcga_go_label,
+                                   tcga_cm_label,
+                                   gtex_label,
+                                   nrow = 3)
 
 main_plot <- (
     cowplot::plot_grid(
-        plot_list[["TCGAGpC5BP"]][['model_gg']] + xlab('') + theme(legend.position = "none"),
-        plot_list[['TCGAGpC5BP']][['ensemble_gg']] + xlab('') + theme(legend.position = "none"),
-        plot_list[['TCGAGpC5BP']][['all_gg']] + xlab('') +
+        plot_list[["TCGAGpC5BP"]][['model_gg']] +
+            xlab('') +
+            theme(legend.position = "none"),
+        plot_list[['TCGAGpC5BP']][['ensemble_gg']] +
+            xlab('') +
+            theme(legend.position = "none"),
+        plot_list[['TCGAGpC5BP']][['all_gg']] +
+            xlab('') +
             geom_point(mapping = aes(x = z_dim,
                                      y = geneset_coverage * 1200),
                        size = 0.5) +
@@ -207,20 +219,41 @@ main_plot <- (
                                                    name = "Coverage (%)"),
                                limits = c(0, 600)) +
             theme(legend.position = "none"),
-        plot_list[["TCGAGpC4CM"]][['model_gg']] + xlab('') + theme(legend.position = "none") + ggtitle(""),
-        plot_list[['TCGAGpC4CM']][['ensemble_gg']] + xlab('') + theme(legend.position = "none") + ggtitle(""),
-        plot_list[['TCGAGpC4CM']][['all_gg']] + xlab('') + theme(legend.position = "none") + ggtitle(""),
-        plot_list[["GTEXGpXCELL"]][['model_gg']] + theme(legend.position = "none") + ggtitle(""),
-        plot_list[['GTEXGpXCELL']][['ensemble_gg']] + theme(legend.position = "none") + ggtitle(""),
-        plot_list[['GTEXGpXCELL']][['all_gg']] + theme(legend.position = "none") + ggtitle(""),
-        labels = c("A", "B", "C", "D", "E", "F", "G", "H", "I"),
+        plot_list[["TCGAGpC4CM"]][['model_gg']] +
+            xlab('') +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        plot_list[['TCGAGpC4CM']][['ensemble_gg']] +
+            xlab('') +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        plot_list[['TCGAGpC4CM']][['all_gg']] +
+            xlab('') +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        plot_list[["GTEXGpXCELL"]][['model_gg']] +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        plot_list[['GTEXGpXCELL']][['ensemble_gg']] +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        plot_list[['GTEXGpXCELL']][['all_gg']] +
+            theme(legend.position = "none") +
+            ggtitle(""),
+        labels = c("a", "", "", "b", "", "", "c", "", ""),
         ncol = 3,
         nrow = 3
     )
 )
 
-main_plot <- cowplot::plot_grid(main_plot, legend, rel_widths = c(1, 0.08), ncol = 2)
-main_plot <- cowplot::plot_grid(dataset_main, main_plot, ncol = 2, rel_widths = c(0.04, 1))
+main_plot <- cowplot::plot_grid(main_plot,
+                                legend,
+                                rel_heights = c(1, 0.08),
+                                nrow = 2)
+main_plot <- cowplot::plot_grid(dataset_main,
+                                main_plot,
+                                ncol = 2,
+                                rel_widths = c(0.04, 1))
 
 main_plot
 
@@ -229,6 +262,7 @@ for(extension in c('.png', '.pdf')) {
     fig_file <- file.path("figures", fig_file)
     cowplot::save_plot(filename = fig_file,
                        plot = main_plot,
-                       base_height = 8,
-                       base_width = 11)
+                       base_height = 150,
+                       base_width = 170,
+                       units = "mm")
 }

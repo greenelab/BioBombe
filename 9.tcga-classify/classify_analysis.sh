d9eb546495105c17a10a1119d9c853290b01d631
gwaygenomics <gregory.way@gmail.com>
2019-01-23 14:12:17 -0500

    add analysis 4 to bash script

--- 9.tcga-classify/classify_analysis.sh
+++ 9.tcga-classify/classify_analysis.sh
@@ -14,3 +14,7 @@ python classify-cancer-types.py
 
 # Analysis 3 - Calculate predictions with uncompressed RNAseq data
 python classify-with-raw-expression.py
+
+# Analysis 4 - Build the top feature matrices and perform classifications with features
+bash build_top_feature_matrices.py
+python classify-using-top-compression-features.py

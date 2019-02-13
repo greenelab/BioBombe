c179d8fdedfad9b42ac93a96252e4fb7320e950d
gwaygenomics <gregory.way@gmail.com>
2019-02-13 13:09:37 -0500

    add separation of TCGA sex

--- 10.detect-separation/scripts/nbconverted/1.separate.py
+++ 10.detect-separation/scripts/nbconverted/1.separate.py
@@ -1,6 +1,12 @@
 
 # coding: utf-8
 
+# # Detect Separation Between Two Phenotypes in TCGA, GTEx, and TARGET Data
+# 
+# **Gregory Way, 2019**
+# 
+# Perform a t-test to isolate the specific k dimension and algorithm that best distinguishes the two groups.
+
 # In[1]:
 
 
@@ -12,6 +18,7 @@ from scipy.stats import ttest_ind, ttest_rel
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+sys.path.append("../8.gtex-interpret")
 from scripts.utils import (
     load_weight_matrix,
     apply_signature,
@@ -23,7 +30,7 @@ sys.path.append("../9.tcga-classify/")
 from scripts.tcga_util import build_feature_dictionary
 
 
-# In[88]:
+# In[2]:
 
 
 def ttest_sex_difference(feature_series, male_ids, female_ids):
@@ -42,21 +49,9 @@ def ttest_sex_difference(feature_series, male_ids, female_ids):
     return([t_stat, t_p, feature_algorithm, feature_num])
 
 
-# In[9]:
+# ## 1. GTEx Sex Analysis
 
-
-get_ipython().system(' wget --directory-prefix="../0.expression-download/download/" "https://storage.googleapis.com/gtex_analysis_v7/annotations/GTEx_v7_Annotations_SubjectPhenotypesDS.txt"')
-
-
-# In[10]:
-
-
-get_ipython().system(' md5sum ../0.expression-download/download/GTEx_v7_Annotations_SubjectPhenotypesDS.txt')
-
-
-# ## GTEx Sex Analysis
-
-# In[2]:
+# In[3]:
 
 
 # Load GTEx phenotype data
@@ -65,13 +60,15 @@ gtex_pheno_df = pd.read_table(file)
 gtex_pheno_df.head()
 
 
-# In[3]:
+# In[4]:
 
 
-gtex_z_matrix_dict = build_feature_dictionary(dataset="GTEX", load_data=True, store_train_test='test')
+gtex_z_matrix_dict = build_feature_dictionary(dataset="GTEX",
+                                              load_data=True,
+                                              store_train_test='test')
 
 
-# In[87]:
+# In[5]:
 
 
 # Extract male and female ids from the dataset
@@ -97,9 +94,10 @@ print(patient_id_df.shape)
 patient_id_df.head()
 
 
-# In[90]:
+# In[6]:
 
 
+# Perform t-test for all compressed features
 full_results = []
 for signal in gtex_z_matrix_dict.keys():
     for z_dim in gtex_z_matrix_dict[signal].keys():
@@ -125,11 +123,13 @@ for signal in gtex_z_matrix_dict.keys():
             full_results.append(result_df)
 
 
-# In[96]:
+# In[7]:
 
 
+# Output results
 full_results_df = pd.concat(full_results)
 full_results_df = full_results_df.assign(neg_log_p=-np.log10(full_results_df.t_p))
+full_results_df = full_results_df.sort_values(by='neg_log_p', ascending=False)
 
 file = os.path.join("results", "sex_separation_gtex_t_test.tsv")
 full_results_df.to_csv(file, sep='\t', index=False)
@@ -138,9 +138,104 @@ print(full_results_df.shape)
 full_results_df.head()
 
 
-# ## TARGET NBL MYCN Status Analysis
+# ## 2. TCGA Sex Analysis
 
-# In[101]:
+# In[8]:
+
+
+# Load TCGA phenotype data
+file = os.path.join("..", "0.expression-download", "download", "TCGA-CDR-SupplementalTableS1.xlsx")
+tcga_pheno_df = pd.read_excel(file)
+
+tcga_pheno_df.head()
+
+
+# In[9]:
+
+
+tcga_z_matrix_dict = build_feature_dictionary(dataset="TCGA",
+                                              load_data=True,
+                                              store_train_test='test')
+
+
+# In[10]:
+
+
+example_matrix_df = tcga_z_matrix_dict['signal']['2']['451283']['test']
+example_matrix_df.head()
+
+
+# In[11]:
+
+
+# Extract male and female ids from the dataset
+patient_id_df = pd.concat(
+    [
+    pd.DataFrame(["{}-{}-{}".format(x[0], x[1], x[2]) for x in example_matrix_df.index.str.split('-')],
+                 columns=['patient_id'])
+        .merge(tcga_pheno_df,
+               how='left',
+               left_on='patient_id',
+               right_on='bcr_patient_barcode'),
+    pd.DataFrame(example_matrix_df.index)
+    ],
+    axis='columns'
+)
+
+males = patient_id_df.query("gender == 'MALE'").sample_id.tolist()
+females = patient_id_df.query("gender == 'FEMALE'").sample_id.tolist()
+
+print(patient_id_df.shape)
+patient_id_df.head()
+
+
+# In[12]:
+
+
+# Perform t-test for all compressed features
+full_results = []
+for signal in tcga_z_matrix_dict.keys():
+    for z_dim in tcga_z_matrix_dict[signal].keys():
+        for seed in tcga_z_matrix_dict[signal][z_dim].keys():
+            z_df = tcga_z_matrix_dict[signal][z_dim][seed]['test']
+            
+            result_df = pd.DataFrame(z_df.apply(lambda x:
+                                                ttest_sex_difference(feature_series=x,
+                                                                     male_ids=males,
+                                                                     female_ids=females)),
+                                     columns = ['result'])
+            
+            result_df = (
+                pd.DataFrame(result_df.result.values.tolist(),
+                             columns=['t_stat', 't_p', 'algorithm', 'feature_num'])
+            ).fillna(1)
+
+            result_df = result_df.assign(
+                z_dim=z_dim,
+                signal=signal,
+                seed=seed
+            )
+            full_results.append(result_df)
+
+
+# In[13]:
+
+
+# Output results
+full_results_df = pd.concat(full_results)
+full_results_df = full_results_df.assign(neg_log_p=-np.log10(full_results_df.t_p))
+full_results_df = full_results_df.sort_values(by='neg_log_p', ascending=False)
+
+file = os.path.join("results", "sex_separation_tcga_t_test.tsv")
+full_results_df.to_csv(file, sep='\t', index=False)
+
+print(full_results_df.shape)
+full_results_df.head()
+
+
+# ## 3. TARGET NBL MYCN Status Analysis
+
+# In[14]:
 
 
 # Load TARGET phenotype data
@@ -149,14 +244,14 @@ nbl_pheno_df = pd.read_table(file)
 nbl_pheno_df.head()
 
 
-# In[108]:
+# In[15]:
 
 
 # Load TARGET matrices
 target_z_matrix_dict = build_feature_dictionary(dataset="TARGET", load_data=True, store_train_test='train')
 
 
-# In[112]:
+# In[16]:
 
 
 # Extract male and female ids from the dataset
@@ -179,14 +274,14 @@ print(patient_id_df.shape)
 patient_id_df.head()
 
 
-# In[116]:
+# In[17]:
 
 
 mycn_amp = patient_id_df.loc[patient_id_df["MYCN status"] == "Amplified", "sample_id"].tolist()
 mycn_nonamp = patient_id_df.loc[patient_id_df["MYCN status"] == "Not Amplified", "sample_id"].tolist()
 
 
-# In[120]:
+# In[18]:
 
 
 full_results = []
@@ -214,11 +309,12 @@ for signal in target_z_matrix_dict.keys():
             full_results.append(result_df)
 
 
-# In[121]:
+# In[19]:
 
 
 full_results_df = pd.concat(full_results)
 full_results_df = full_results_df.assign(neg_log_p=-np.log10(full_results_df.t_p))
+full_results_df = full_results_df.sort_values(by='neg_log_p', ascending=False)
 
 file = os.path.join("results", "nbl_mycn_separation_target_t_test.tsv")
 full_results_df.to_csv(file, sep='\t', index=False)

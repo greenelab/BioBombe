4e1c623c9461a13bc62f0801662f9e2e812780c7
Greg Way <gregory.way@gmail.com>
2019-02-15 14:15:25 -0500

    Updating Coefficient Interpretation to All Feature Ensemble (#167)
    * update to interpreting the coefficients in the all ensemble model
    
    * add output tables
    
    * remove old file
    
    * shorten the title and add description

--- 9.tcga-classify/scripts/nbconverted/interpret-all-feature-ensemble-model.py
+++ 9.tcga-classify/scripts/nbconverted/interpret-all-feature-ensemble-model.py
@@ -1,11 +1,11 @@
 
 # coding: utf-8
 
-# ## Generating a Table of the BioBombe Interpreted Features in a High Scoring Ensemble model of TP53 inactivation
+# ## Interpreting Ensemble Compressed Features
 # 
 # **Gregory Way, 2019**
 # 
-# I use the model previously identified that was used to predict TP53 inactivation.
+# The following notebook will assign biological knowledge to the compressed features using the network projection approach. I use the model previously identified that was used to predict TP53 inactivation.
 # I observe the BioBombe gene set enrichment scores for the features with high coefficients in this model.
 
 # In[1]:
@@ -16,12 +16,12 @@ import sys
 import pandas as pd
 
 
-# ## Load the Top Model Identified Previously
+# ## Load the `All Feature` Ensemble Model
 
 # In[2]:
 
 
-model_file = os.path.join("results", "top_model_ensemble_tp53_feature_for_followup.tsv")
+model_file = os.path.join("results", "top_model_ensemble_all_features_tp53_feature_for_followup.tsv")
 top_model_df = pd.read_table(model_file)
 top_model_df
 
@@ -29,115 +29,152 @@ top_model_df
 # In[3]:
 
 
-# The seed we used to compile single model
-seed = "165158"
-z_dim = top_model_df.z_dim.values[0]
+coef_file = os.path.join("results",
+                         "mutation_ensemble_all",
+                         "TP53",
+                         "TP53_ensemble_all_features_coefficients.tsv.gz")
+coef_df = pd.read_table(coef_file).drop(['signal', 'z_dim', 'seed', 'algorithm'], axis='columns')
+coef_df.head()
 
-
-# ## Load the BioBombe network projection results for Cancer Hallmarks
 
 # In[4]:
 
 
-file = os.path.join("..", "6.biobombe-projection", "results", "tcga",
-                    "gph", "signal", "tcga_z_200_GpH__geneset_scores.tsv.gz")
-
-scores_df = (
-    pd
-    .read_table(file)
-    .query("seed == @seed")
-    .query("z == @z_dim")
+full_coef_id_df = (
+    pd.DataFrame(coef_df.feature.str.split("_").values.tolist(),
+                 columns=['algorithm', 'individual_feature', 'seed', 'k', 'signal'])
 )
 
-scores_df = (
-    scores_df
-    .assign(full_feature=scores_df.algorithm.astype(str) + "_" + scores_df.feature.astype(str),
-            abs_z_score=scores_df.z_score.abs())
-)
-scores_df.head()
+full_coef_id_df = pd.concat([full_coef_id_df, coef_df], axis='columns')
+full_coef_id_df = full_coef_id_df.query("abs > 0").query("signal == 'signal'")
+
+print(full_coef_id_df.shape)
+full_coef_id_df.head()
 
 
-# ## Load Model Coefficients
+# ## Load Network Projection Results
 
 # In[5]:
 
 
-file = os.path.join("results",
-                    "mutation_ensemble",
-                    "TP53",
-                    "TP53_ensemble_all_alg_coefficients.tsv.gz")
-
-top_n_features = 10
-
-coef_df = (
-    pd.read_table(file)
-    .query("seed == @seed")
-    .query("z_dim == @z_dim")
-    .query("signal == 'signal'")
-    .sort_values(by='abs', ascending=False)
-    .head(top_n_features)
-    .reset_index(drop=True)
-)
-
-# Rename columns
-coef_extract_df = (
-    pd.DataFrame(coef_df.feature.str.split('_').values.tolist(),
-                 columns=['feature_alg', 'feature_num',
-                          'feature_seed', 'feature_z',
-                          'feature_signal'])
-)
-
-coef_extract_df = (
-    coef_extract_df
-    .assign(use_feature=coef_extract_df.feature_alg + "_" + coef_extract_df.feature_num)
-)
-
-coef_df = pd.concat([coef_df, coef_extract_df], axis='columns')
-
-use_features = coef_df.use_feature.tolist()
-coef_df
+gph_dir = os.path.join("..",
+                       "6.biobombe-projection",
+                       "results",
+                       "tcga",
+                       "gph",
+                       "signal")
+gph_files = os.listdir(gph_dir)
 
 
 # In[6]:
 
 
-# Explore the biobombe scores for specific DAE features
-top_n_features = 10
-
-biobombe_df = (
-    scores_df
-    .query("full_feature in @use_features")
-    .merge(coef_df,
-           how='left',
-           left_on=['full_feature', 'algorithm', 'seed'],
-           right_on=['use_feature', 'feature_alg', 'seed'])
-    .drop(['model_type', 'feature_x', 'feature_y', 'signal'], axis='columns')
-    .sort_values(by=['abs', 'abs_z_score'], ascending=False)
-    .reset_index(drop=True)
-)
-
-top_biobombe_df = (
-    biobombe_df
-    .groupby('full_feature')
-    .apply(func=lambda x: x.abs_z_score.nlargest(top_n_features))
-    .reset_index()
-    .merge(biobombe_df
-           .reset_index(),
-           right_on=['index', 'abs_z_score', 'full_feature'],
-           left_on=['level_1', 'abs_z_score', 'full_feature'])
-    .drop(['level_1', 'index'], axis='columns')
-    .sort_values(by=['weight', 'z_score'], ascending=False)
-)
-    
-    
-print(top_biobombe_df.shape)
-top_biobombe_df
+all_scores_list = []
+for file in gph_files:
+    file = os.path.join(gph_dir, file)
+    scores_df = pd.read_table(file)
+    all_scores_list.append(scores_df)
 
 
 # In[7]:
 
 
-# Output biobombe scores applied to high scoring DAE features
-file = os.path.join('results', 'tcga_tp53_classify_top_biobombe_scores_ensemble_model_table.tsv')
-top_biobombe_df.to_csv(file, sep='\t', index=False)
+all_scores_df = pd.concat(all_scores_list, axis='rows')
+
+print(all_scores_df.shape)
+all_scores_df.head()
+
+
+# In[8]:
+
+
+all_scores_df = all_scores_df.assign(big_feature_id=all_scores_df.algorithm + "_" +
+                                     all_scores_df.feature.astype(str) + "_" +
+                                     all_scores_df.seed.astype(str) + "_" +
+                                     all_scores_df.z.astype(str) + "_signal")
+all_scores_df = all_scores_df.assign(abs_z_score=all_scores_df.z_score.abs())
+
+
+# In[9]:
+
+
+all_coef_scores_df = (
+    full_coef_id_df
+    .merge(all_scores_df,
+           how='left',
+           left_on="feature",
+           right_on="big_feature_id")
+    .sort_values(by=['abs', 'abs_z_score'], ascending=False)
+    .reset_index(drop=True)
+)
+
+all_coef_scores_df.head()
+
+
+# In[10]:
+
+
+# Explore the biobombe scores for specific DAE features
+top_n_features = 5
+
+biobombe_df = (
+    all_coef_scores_df
+    .groupby('big_feature_id')
+    .apply(func=lambda x: x.abs_z_score.nlargest(top_n_features))
+    .reset_index()
+    .merge(all_coef_scores_df
+           .reset_index(),
+           right_on=['index', 'abs_z_score', 'big_feature_id'],
+           left_on=['level_1', 'abs_z_score', 'big_feature_id'])
+    .drop(['level_1', 'index', 'feature_x',
+           'algorithm_x', 'seed_x',
+           'model_type', 'algorithm_y',
+           'feature_y', 'seed_y', 'z'], axis='columns')
+    .sort_values(by=['abs', 'abs_z_score'], ascending=False)
+    .reset_index(drop=True)
+)
+
+print(biobombe_df.shape)
+biobombe_df.head(20)
+
+
+# In[11]:
+
+
+# Output biobombe scores applied to the all feature ensemble model
+file = os.path.join('results', 'tcga_tp53_classify_top_biobombe_scores_all_feature_ensemble_model_table.tsv')
+biobombe_df.to_csv(file, sep='\t', index=False)
+
+
+# ## Detect the highest contributing variables
+
+# In[12]:
+
+
+neg_biobombe_df = biobombe_df.query("weight < 0")
+pos_biobombe_df = biobombe_df.query("weight > 0")
+
+top_neg_variables_df = neg_biobombe_df.groupby("variable")['weight'].sum().sort_values(ascending=True)
+top_pos_variables_df = pos_biobombe_df.groupby("variable")['weight'].sum().sort_values(ascending=False)
+
+
+# In[13]:
+
+
+full_result_df = pd.DataFrame(pd.concat([top_pos_variables_df, top_neg_variables_df]))
+full_result_df = (
+    full_result_df
+    .assign(abs_weight=full_result_df.weight.abs())
+    .sort_values(by='abs_weight', ascending=False)
+)
+
+full_result_df.head()
+
+
+# In[14]:
+
+
+# Output biobombe scores applied to the all feature ensemble model
+file = os.path.join('results', 'tcga_tp53_classify_aggregate_biobombe_scores_all_feature_ensemble.tsv')
+full_result_df.to_csv(file, sep='\t', index=False)
 
